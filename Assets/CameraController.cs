35afeb6c38be042889ee6601f28cc7c30f612cf5
Jzp <jiazhipeng03@163.com>
2015-11-05 17:34:13 +0800

    Add camera and player move control
    在player底部添加额外collider，根据collider是否和platform碰撞，判断是否在地面能跳起。
    
    遗留问题：额外collider比player实际宽度小（为了避免和platform在空中横向碰撞），导致player在platform边缘时可站立，无法起跳。

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-
+    [SerializeField]
+    GameObject m_Player;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Vector2 bound = new Vector2(0, Mathf.Infinity);
+        //  TO-DO set max bound
+        transform.position = new Vector3(Mathf.Max(bound[0],m_Player.transform.position.x), transform.position.y, transform.position.z);
+    }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float m_Speed = 10f;
+    [SerializeField]
+    private int m_JumpForce = 500;
+
+    private Rigidbody2D m_RigidBody2D;
+    private bool m_bJump = false;
+    private bool m_bOnGround = false;
+	// Use this for initialization
+	void Start () {
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float fX = Input.GetAxis("Horizontal");
+        m_RigidBody2D.velocity = new Vector2(fX * m_Speed, m_RigidBody2D.velocity.y);
+        if (m_bOnGround && Input.GetButtonDown("Jump"))
+        {
+            m_bJump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (m_bOnGround && m_bJump)
+        {            
+            m_RigidBody2D.AddForce(new Vector2(0, m_JumpForce));
+            m_bJump = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "platform")
+            m_bOnGround = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "platform")
+            m_bOnGround = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "platform")
+            m_bOnGround = false;
+    }
+}
